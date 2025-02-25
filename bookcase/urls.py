20b0ddb6f6608f053d6fa7feeca99143929bf2d0
vmr093 <ranjha.viola@gmail.com>
2025-02-25 12:55:31 -0600

    fixing syntax error

--- bookcase/urls.py
+++ bookcase/urls.py
@@ -1,4 +1,4 @@
-ffrom django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView  # <-- ADD THIS IMPORT
